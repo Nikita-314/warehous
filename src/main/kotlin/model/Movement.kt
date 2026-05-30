2426977bba74f444db6db7bc32ff2d58ad247475
Nikita-314 <leonovn70@gmail.com>
2026-05-30 21:03:44 +0300

    изменил перемещения

--- src/main/kotlin/model/Movement.kt
+++ src/main/kotlin/model/Movement.kt
@@ -4,8 +4,10 @@ data class Movement(
     val inventoryId: String, // Какое оборудование перемещали
     val date: String,
     val documentNumber: String,
-    val fromGroup: String, // Откуда передали
-    val toGroup: String, // Куда передали
+    //val fromGroup: String, // Откуда передали
+    //val toGroup: String, // Куда передали
+    val source: String, // Откуда / Источник
+    val destination: String, // Куда / Получатель / Пункт назначения
     val location: String?, // Объект эксплуатации
     val transferredBy: String, // Кто передал
     val acceptedBy: String // Кто принял
